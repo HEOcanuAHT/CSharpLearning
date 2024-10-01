32e92e4eed635c75d72038e5ebee5f1dbd77e4c6
Datskov Andrey <dackov.ai@dns-shop.ru>
2024-10-01 12:27:15 +1000

    Добавьте файлы проекта.

---
+++ деление (с остатком)/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace деление__с_остатком_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите делимое: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите делитель: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} = {quotient} остаток {remainder}");
+        }
+    }
+}
